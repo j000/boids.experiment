905e48db983ec253b2d56e1c5bb8673e5dae18c1
j000 <jarymut+github@gmail.com>
2016-06-30 16:49:55 +0200

    Add CMake black magic
    - CMakeLists.txt
    - cmake directory
    - version.h
    
    I'm still not good with cmake, so expect a lot of mistakes and changes.

--- boids.c
+++ boids.c
@@ -1,4 +1,9 @@
+#include "version.h"
+#include <stdio.h>
+
 int main (int argc, char *argv[], char **env_var_ptr) {
 
+	printf ("%s v%s.%s-%s\n", gVERSION, gVERSION_MAJOR, gVERSION_MINOR,
+		gVERSION_REST);
 	return 0;
 }
---
+++ version.h
@@ -0,0 +1,10 @@
+#ifndef VERSION_H
+#  define VERSION_H
+
+//Global version strings
+extern const char *gVERSION;
+extern const char *gVERSION_MAJOR;
+extern const char *gVERSION_MINOR;
+extern const char *gVERSION_REST;
+
+#endif //VERSION_H
