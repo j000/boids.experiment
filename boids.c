d6caec2784dd6212a35e4924e2abf53480b20dc2
j000 <jarymut+github@gmail.com>
2016-06-30 23:55:34 +0200

    Add SDL2 header and library to linker.
    Add first SDL functions: SDL_Init, SDL_Quit.

--- boids.c
+++ boids.c
@@ -1,9 +1,17 @@
 #include "version.h"
+#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char *argv[], char **env_var_ptr) {
-
 	printf ("%s v%s.%s-%s\n", gVERSION, gVERSION_MAJOR, gVERSION_MINOR,
 		gVERSION_REST);
-	return 0;
+	if (SDL_Init (SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+		fprintf (stderr, "Unable to initialize SDL:  %s\n",
+			 SDL_GetError ());
+		exit(1);
+	}
+	atexit (SDL_Quit);
+	/* ... */
+	exit(0);
 }
