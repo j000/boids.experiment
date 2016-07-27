f7dcb67f9d12c9dc0197bead22c5e13c74db2407
j000 <jarymut+github@gmail.com>
2016-07-27 15:31:36 +0200

    [Appveyor] Hopefully squash build.bat bugs

--- build.bat
+++ build.bat
@@ -1,20 +1,17 @@
 REM @ECHO OFF
-ECHO debug test
-where clang-cl
-ECHO debug test2
-IF NOT where /q clang-cl (
+IF NOT WHERE /Q clang-cl (
 	ECHO "*** LLVM not detected ***"
 	ECHO "Please install LLVM and try again."
 	ECHO "http://llvm.org/releases/download.html"
 	GOTO:eof
 )
-IF NOT where /q cmake (
+IF NOT WHERE /Q cmake (
 	ECHO "*** Cmake not detected ***"
 	ECHO "Please install Cmake and try again."
 	ECHO "https://cmake.org/download/#latest"
 	GOTO:eof
 )
-IF NOT where /q msbuild (
+IF NOT WHERE /Q msbuild (
 	ECHO "*** MSBuild not detected ***"
 	ECHO "Please install Visual Studio and try again."
 	GOTO:eof
